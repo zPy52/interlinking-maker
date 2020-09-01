756a8ffab7a38274437717fb233c003150b7e5eb
zJar84 <36101760+zJar84@users.noreply.github.com>
2020-09-02 00:00:54 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicio</title>
+</head>
+<body>
+
+	<form method="POST" action="./hacedor.php">
+		
+		<textarea name="form_text" cols="80" rows="10" placeholder="<?php echo 'actor, 10000000;' . "\n" . 'hobby, 2000;'; ?>"></textarea>
+
+        <br/>
+
+		<input type="submit" value="Enviar"/>
+
+	</form>
+
+</body>
+</html>
